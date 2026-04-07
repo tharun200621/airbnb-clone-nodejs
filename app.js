b8650329788adaea4e9ccf7be1d7718815ff716c
tharun200621 <tharunm1981@gmail.com>
2026-04-07 18:12:34 +0530

    add completed files

--- app.js
+++ app.js
@@ -15,11 +15,12 @@ const listingRouter=require("./routes/listing.js");
 const reviewRouter=require("./routes/review.js");
 const userRouter=require("./routes/user.js")
 const session=require("express-session");
+const MongoStore = require('connect-mongo').default;
 const flash=require("connect-flash");
 const passport=require("passport");
 const LocalStrategy=require("passport-local").Strategy;
 const User=require("./MODELS/user.js");
-
+const dbUrl=process.env.ATLASDB_URL;
 
 main().then(()=>{
    console.log("connected to db");
@@ -28,7 +29,7 @@ main().then(()=>{
     console.log(err);
 });
 async function main(){
-    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+    await mongoose.connect(dbUrl);
 }
 app.engine("ejs", ejsMate);
 app.set("view engine","ejs");
@@ -37,7 +38,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname,"/public")));
 
+
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET,
+  },
+  touchAfter: 24 * 3600,
+});
+store.on("error",(err)=>{
+    console.log("ERROR IN MONGOSTORE",err);
+});
+
 const sessionOptions={
+    store,
     secret:"superSecret",
     resave:false,
     saveUninitialized:true,
@@ -55,18 +69,21 @@ app.get("/",(req,res)=>{
 app.use(session(sessionOptions));
 app.use(flash());
 
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
 app.use((req,res,next)=>{
     res.locals.success=req.flash("success");
     res.locals.error=req.flash("error");
     res.locals.curUser=req.user;
     next();
 });
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 
 // app.get("/demouser",async (req,res)=>{
 //    let fakeUser=new User({
